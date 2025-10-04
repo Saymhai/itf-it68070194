064007753f01aaa09c2912725b59ac3ee5db48a9
Saymhai <151415377+Saymhai@users.noreply.github.com>
2025-10-05 02:51:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const CAB = document.getElementById("CAB")
 const CCB = document.getElementById("CCB")
+const BO = document.getElementById("BO")
+const action = document.getElementById("action")
 
-function add() {
-    CAB.value = Number(CAB.value) + 1000
+let account = 1000
+let cash = 1000
+let n = 1
+let massage = "1, Current account balance: 1000, Current cash balance: 1000"
+
+function changeCurrent() {
+    account = Number(CAB.value)
+    cash = Number(CCB.value)
+    n += 1
+    massage = n + ", Current account balance: " + account + ", Current cash balance: " + cash + "\n" + massage;
+    document.getElementById("widget").innerText = massage;
+}
+
+function operationProceed() {
+    n += 1
+    if (action.value === 'deposit') {
+        if (Number(BO.value) > cash){
+            massage = n + ", Cloundn't deposit entered balance. (Insufficient cash balance)" + "\n" + massage;
+        } else {
+            account += Number(BO.value);
+            cash -= Number(BO.value);
+            massage = n + ", Current account balance: " + account + ", Current cash balance: " + cash + "\n" + massage;
+        }
+    } else {
+        if (Number(BO.value) > account){
+            massage = n + ", Cloundn't withdraw entered balance. (Insufficient account balance)" + "\n" + massage;
+        } else {
+            account -= Number(BO.value);
+            cash += Number(BO.value);
+            massage = n + ", Current account balance: " + account + ", Current cash balance: " + cash + "\n" + massage;
+        }
+    }
+    document.getElementById("widget").innerText = massage;
+}
+
+function currencyConvert() {
+
 }
